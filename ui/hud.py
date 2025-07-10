53f5f4329b6502ea243fd2dff866da8247b19b49
Xivalor <yegor.drozd2006@gmail.com>
2025-07-10 04:20:58 +0300

    Skill icons and animations

--- ui/hud.py
+++ ui/hud.py
@@ -3,6 +3,7 @@ from PySide6.QtWidgets import QWidget, QHBoxLayout, QLabel, QVBoxLayout, QProgre
 from PySide6.QtCore import Qt
 from config import *
 from resources.colors import *
+from PySide6.QtGui import QPainter, QPixmap
 
 class HUD(QWidget):
     def __init__(self, parent=None):
@@ -10,6 +11,10 @@ class HUD(QWidget):
         self.setFixedHeight(WINDOW_HEIGHT - ROOM_SIZE[0])
         self.setFixedWidth(WINDOW_WIDTH)
         self.setStyleSheet("background-color: rgba(0, 0, 0, 180);")
+        
+        self.power_chord_pixmap = QPixmap("resources/images/icons/axe_guitar.png")
+        self.major_chord_pixmap = QPixmap("resources/images/icons/blaster_guitar.png")
+        self.minor_chord_pixmap = QPixmap("resources/images/icons/bomb_amp.png")
 
         main_layout = QVBoxLayout(self)
         main_layout.setContentsMargins(10, 5, 10, 5)
@@ -18,17 +23,42 @@ class HUD(QWidget):
         hp_row = QHBoxLayout()
         
         # Скилы        
-        self.power_chord_label = QLabel("Power chord")
-        self.major_chord_label = QLabel("Major chord")
-        self.minor_chord_label = QLabel("Minor chord")
+        self.power_chord_widget = QWidget()
+        self.power_chord_layout = QHBoxLayout(self.power_chord_widget)
+        self.power_chord_layout.setContentsMargins(0, 0, 0, 0)
+        self.power_chord_text = QLabel()
+        self.power_chord_icon = QLabel()
+        self.power_chord_icon.setPixmap(self.power_chord_pixmap)
+        self.power_chord_text.setText("1")
+        self.power_chord_layout.addWidget(self.power_chord_icon)
+        self.power_chord_layout.addWidget(self.power_chord_text)
+        self.set_chosen_skill_style(self.power_chord_widget)
         
-        self.power_chord_label.setStyleSheet(f"background-color: {CHOSEN_WEAPON_COLOR}; font-size: 16px;")
-        self.major_chord_label.setStyleSheet(f"background-color: {NOT_CHOSEN_WEAPON_COLOR}; font-size: 16px;")
-        self.minor_chord_label.setStyleSheet(f"background-color: {NOT_CHOSEN_WEAPON_COLOR}; font-size: 16px;")
-            
-        weapon_row.addWidget(self.power_chord_label)
-        weapon_row.addWidget(self.major_chord_label)
-        weapon_row.addWidget(self.minor_chord_label)
+        self.major_chord_widget = QWidget()
+        self.major_chord_layout = QHBoxLayout(self.major_chord_widget)
+        self.major_chord_layout.setContentsMargins(0, 0, 0, 0)
+        self.major_chord_text = QLabel()
+        self.major_chord_icon = QLabel()
+        self.major_chord_icon.setPixmap(self.major_chord_pixmap)
+        self.major_chord_text.setText("2")
+        self.major_chord_layout.addWidget(self.major_chord_icon)
+        self.major_chord_layout.addWidget(self.major_chord_text)
+        self.set_not_chosen_skill_style(self.major_chord_widget)
+        
+        self.minor_chord_widget = QWidget()
+        self.minor_chord_layout = QHBoxLayout(self.minor_chord_widget)
+        self.minor_chord_layout.setContentsMargins(0, 0, 0, 0)
+        self.minor_chord_text = QLabel()
+        self.minor_chord_icon = QLabel()
+        self.minor_chord_icon.setPixmap(self.minor_chord_pixmap)
+        self.minor_chord_text.setText("3")
+        self.minor_chord_layout.addWidget(self.minor_chord_icon)
+        self.minor_chord_layout.addWidget(self.minor_chord_text)
+        self.set_not_chosen_skill_style(self.minor_chord_widget)
+        
+        weapon_row.addWidget(self.power_chord_widget)
+        weapon_row.addWidget(self.major_chord_widget)
+        weapon_row.addWidget(self.minor_chord_widget)
 
         # Жизнь
         self.hp_bar = QProgressBar()
@@ -90,15 +120,46 @@ class HUD(QWidget):
     
     def update_chord(self, number):
         if(number == 1):
-            self.power_chord_label.setStyleSheet(f"background-color: {CHOSEN_WEAPON_COLOR}; font-size: 16px;")
-            self.major_chord_label.setStyleSheet(f"background-color: {NOT_CHOSEN_WEAPON_COLOR}; font-size: 16px;")
-            self.minor_chord_label.setStyleSheet(f"background-color: {NOT_CHOSEN_WEAPON_COLOR}; font-size: 16px;")
+            self.set_chosen_skill_style(self.power_chord_widget)
+            self.set_not_chosen_skill_style(self.major_chord_widget)
+            self.set_not_chosen_skill_style(self.minor_chord_widget)
         elif(number == 2):
-            self.power_chord_label.setStyleSheet(f"background-color: {NOT_CHOSEN_WEAPON_COLOR}; font-size: 16px;")
-            self.major_chord_label.setStyleSheet(f"background-color: {CHOSEN_WEAPON_COLOR}; font-size: 16px;")
-            self.minor_chord_label.setStyleSheet(f"background-color: {NOT_CHOSEN_WEAPON_COLOR}; font-size: 16px;")
+            self.set_not_chosen_skill_style(self.power_chord_widget)
+            self.set_chosen_skill_style(self.major_chord_widget)
+            self.set_not_chosen_skill_style(self.minor_chord_widget)
         elif(number == 3):
-            self.power_chord_label.setStyleSheet(f"background-color: {NOT_CHOSEN_WEAPON_COLOR}; font-size: 16px;")
-            self.major_chord_label.setStyleSheet(f"background-color: {NOT_CHOSEN_WEAPON_COLOR}; font-size: 16px;")
-            self.minor_chord_label.setStyleSheet(f"background-color: {CHOSEN_WEAPON_COLOR}; font-size: 16px;")
-
+            self.set_not_chosen_skill_style(self.power_chord_widget)
+            self.set_not_chosen_skill_style(self.major_chord_widget)
+            self.set_chosen_skill_style(self.minor_chord_widget)
+            
+    def set_chosen_skill_style(self, widget):
+        widget.setFixedSize(240, 130)
+        widget.setStyleSheet(f"""
+            QWidget {{
+                background-color: {CHOSEN_WEAPON_COLOR};
+                border: none;
+                border-radius: 8px;
+                padding: 8px 15px;
+            }}
+            QLabel {{
+                color: white;
+                font-size: 48px;
+                font-weight: bold;
+            }}
+        """)
+        
+    def set_not_chosen_skill_style(self, widget):
+        widget.setFixedSize(230, 120)
+        widget.setStyleSheet(f"""
+            QWidget {{
+                background-color: {NOT_CHOSEN_WEAPON_COLOR};
+                border: none;
+                border-radius: 8px;
+                padding: 8px 15px;
+            }}
+            QLabel {{
+                color: white;
+                font-size: 48px;
+                font-weight: bold;
+            }}
+        """)
