c2dee72ebf58325d14ce136a1539817e41c07de0
Ngd_Mav <riherr00@gmail.com>
2025-07-11 08:18:47 +0300

    visual fix, volume style

--- ui/menu_pause.py
+++ ui/menu_pause.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QWidget, QPushButton, QVBoxLayout, QLabel, QSpacerItem, QSizePolicy, QSlider
+from PySide6.QtWidgets import QWidget, QPushButton, QVBoxLayout, QLabel, QSpacerItem, QSizePolicy, QSlider, QHBoxLayout
 from PySide6.QtCore import Signal, Qt
 from PySide6.QtGui import QColor
 from pathlib import Path
@@ -32,27 +32,39 @@ class PauseMenu(QWidget):
 
         layout = QVBoxLayout(self)
         layout.setContentsMargins(30, 30, 30, 30)
-        layout.setSpacing(20)
+        layout.setSpacing(15)
         layout.addWidget(title)
-        layout.addSpacerItem(QSpacerItem(0, 20, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding))
+        layout.addSpacing(10)
         layout.addWidget(self.resumeButton)
         layout.addWidget(self.exitButton)
-        layout.addSpacerItem(QSpacerItem(0, 20, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding))
+        layout.addSpacing(10)
+
+        music_layout = QHBoxLayout()
+        music_label = QLabel("Music Volume")
+        music_label.setFixedWidth(100)
 
         self.music_slider = QSlider(Qt.Horizontal)
         self.music_slider.setRange(0, 100)
         self.music_slider.setValue(int(music.music_volume * 100))
+        self.music_slider.setMaximumWidth(200)
         self.music_slider.valueChanged.connect(lambda v: music.set_volume(music_volume=v / 100))
+
+        music_layout.addWidget(music_label)
+        music_layout.addWidget(self.music_slider)
+        layout.addLayout(music_layout)
+
+        sfx_layout = QHBoxLayout()
+        sfx_label = QLabel("SFX Volume")
+        sfx_label.setFixedWidth(100)
 
         self.sfx_slider = QSlider(Qt.Horizontal)
         self.sfx_slider.setRange(0, 100)
         self.sfx_slider.setValue(int(music.sfx_volume * 100))
+        self.sfx_slider.setMaximumWidth(200)
         self.sfx_slider.valueChanged.connect(lambda v: music.set_volume(sfx_volume=v / 100))
 
-        music_label = QLabel("Music Volume")
-        sfx_label = QLabel("SFX Volume")
+        sfx_layout.addWidget(sfx_label)
+        sfx_layout.addWidget(self.sfx_slider)
+        layout.addLayout(sfx_layout)
 
-        layout.addWidget(music_label)
-        layout.addWidget(self.music_slider)
-        layout.addWidget(sfx_label)
-        layout.addWidget(self.sfx_slider)
+        layout.addStretch()
